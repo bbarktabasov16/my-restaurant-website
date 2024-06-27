14e32d1d12444d48f94e220e8644466106d22e61
Bekzat Baryktabasov <bbarktabasov16@gmail.com>
2024-06-27 18:51:16 +0600

    update project

--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // Modal
 
   const modalTrigger = document.querySelectorAll("[data-modal]"),
-    modal = document.querySelector(".modal")
+    modal = document.querySelector(".modal");
 
   function openModal() {
     modal.classList.add("show");
@@ -188,7 +188,7 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   modal.addEventListener("click", (e) => {
-    if (e.target === modal || e.target.getAttribute('data-close') == '') {
+    if (e.target === modal || e.target.getAttribute("data-close") == "") {
       closeModal();
     }
   });
@@ -264,111 +264,114 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    4,
-    ".menu .container"
-    // "menu__item"
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки,но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    6,
-    ".menu .container",
-    "menu__item"
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    5,
-    ".menu .container",
-    "menu__item"
-  ).render();
+    return await res.json();
+  };
+
+  // getResource("http://localhost:3000/menu").then((data) => {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //   });
+  // });
+
+  axios.get("http://localhost:3000/menu").then((data) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
 
   // Forms
 
   const forms = document.querySelectorAll("form");
 
   const message = {
-    loading: 'img/form/spinner.svg',
+    loading: "img/form/spinner.svg",
     success: "Спасибо! Скоро мы с вами свяжемся",
     failure: "Что-то пошло не так...",
   };
 
-	forms.forEach(item => {
-		postData(item)
-	})
+  forms.forEach((item) => {
+    bindPostData(item);
+  });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: data,
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
 
       const statusMessage = document.createElement("img");
-			statusMessage.src = message.loading
+      statusMessage.src = message.loading;
       statusMessage.style.cssText = `
 				display: block;
 				margin: 0 auto;
 			`;
-			form.insertAdjacentElement('afterend', statusMessage)
+      form.insertAdjacentElement("afterend", statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-
-      request.setRequestHeader("Content-type", "application/json");
       const formData = new FormData(form);
 
-			const object = {}
-			formData.forEach(function(value, key) {
-				object[key] = value
-			})
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			const json = JSON.stringify(object)
-
-      request.send(json);
-
-      request.addEventListener("load", () => {
-        if (request.status === 200) {
-					showThanksModal(message.success);
-					form.reset()
-					statusMessage.remove()
-        } else {
-					showThanksModal(message.failure);
-				}
-      });
+      postData("http://localhost:3000/requests", json)
+        .then((data) => {
+          console.log(data);
+          showThanksModal(message.success);
+          statusMessage.remove();
+        })
+        .catch(() => {
+          showThanksModal(message.failure);
+        })
+        .finally(() => {
+          form.reset();
+        });
     });
   }
 
-	function showThanksModal(message) {
-		const prevModalDialog = document.querySelector('.modal__dialog')
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector(".modal__dialog");
 
-		prevModalDialog.classList.add('hide')
-		openModal()
+    prevModalDialog.classList.add("hide");
+    openModal();
 
-		const thanksModal = document.createElement('div')
-		thanksModal.classList.add('modal__dialog')
-		thanksModal.innerHTML = `
+    const thanksModal = document.createElement("div");
+    thanksModal.classList.add("modal__dialog");
+    thanksModal.innerHTML = `
 			<div class="modal__content">
 				<div class="modal__close" data-close>×</div>
 				<div class="modal__title">${message}</div>
 			</div>
-		`
+		`;
 
-		document.querySelector('.modal').append(thanksModal)
+    document.querySelector(".modal").append(thanksModal);
 
-		setTimeout(() => {
-			thanksModal.remove()
-			prevModalDialog.classList.add('show')
-			prevModalDialog.classList.remove('hide')
-			closeModal()
-		}, 4000)
-	}
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add("show");
+      prevModalDialog.classList.remove("hide");
+      closeModal();
+    }, 4000);
+  }
 });
